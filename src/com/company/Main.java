e7accf45d779166183a50537301396c419c8cc54
roberto <usr2266@salesianos-lacuesta.net>
2022-02-10 19:32:53 +0000

    Feature: "Envia el nombre de usuario la primera vez que conecta, recibe la lista de mensaje y comienza a escribir hasta que recibe "good bye" del servidor"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -16,9 +19,38 @@ public class Main {
         oos = new ObjectOutputStream(socket.getOutputStream());
         ois = new ObjectInputStream(socket.getInputStream());
 
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> msgList = new ArrayList<String>();
+
         try{
-            oos.writeObject("Juan");
-            System.out.println((String) ois.readObject());
+            //envio nombre de usuario
+            System.out.println("Introduce tu nombre de usuario");
+            String username = scan.nextLine();
+            oos.writeObject(username);
+            //recibo el mensaje del servidor de bienvenida
+            String msg = "";
+            boolean keepTalking = false;
+            boolean justConnected = true;
+
+            while (!keepTalking) {
+                if(justConnected){
+                    System.out.println(ois.readObject());
+                    justConnected = false;
+                }
+                //envio mensajes y compruebo que el usuario no quiere terminar
+                System.out.println("Escribe el mensaje ");
+                msg = scan.nextLine();
+
+                if(!msg.equals("good bye")) {
+                    //Envío el mensaje al servidor y espero la respuesta
+                    oos.writeObject(msg);
+
+                    //msgList.add((String) ois.readObject());
+                    System.out.println(ois.readObject());
+                } else {
+                    keepTalking = true;
+                }
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
